e2b38412a3d98ff967b0784aedcaaf29e5e5660e
WILL MEIER <willmeierart@gmail.com>
2017-11-24 17:16:03 -0700

    initial commit

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import Layout from "../components/architecture/Layout"
+
+export default () => (
+  <Layout title="About">
+    <div>About</div>
+  </Layout>
+);
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Layout from '../components/architecture/Layout'
+
+export default () => (
+  <Layout title='Home'>
+    <div>Home</div>
+  </Layout>
+)
